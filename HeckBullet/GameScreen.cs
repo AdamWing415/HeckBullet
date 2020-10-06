5df0283f6d81564b7db57aa9d7f25128c593ac16
Adam Wingert <adamwing415@ed.amdsb.ca>
2020-10-06 08:56:33 -0400

    Added a noise

--- HeckBullet/GameScreen.cs
+++ HeckBullet/GameScreen.cs
@@ -143,6 +143,7 @@ namespace HeckBullet
             //checks if teh boss is at half health
             if (BossHealth == 1000)
             {
+                explode.Play();
                 ships[boss].image = Resources.boss_damaged;
                 bulletSpeed = 7;
                 Lines();
